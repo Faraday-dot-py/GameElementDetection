4f2985587c6fbd4a8a7bb3c52c8b7351a6e688f4
Adam Webb <adam.webb@mrjaffesclass.com>
2023-05-05 12:47:06 -0700

    Basic setup and data confirmation

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ from detecto.utils import read_image
 image = read_image('image.jpg')
 plt.imshow(image)
 plt.show()
+
+
+from detecto.core import Dataset
+
+dataset = Dataset('./labels/', './images/')
+
+from detecto.visualize import show_labeled_image
+
+image, targets = dataset[0]
+show_labeled_image(image, targets['boxes'], targets['labels'])
